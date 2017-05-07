4c2c601fef6679f30b4433c7fc116b05e7cbf349
cheyuexian <694547990@qq.com>
2017-05-07 23:22:15 +0800

    Test
    My first java program

--- Test/Test.java
+++ Test/Test.java
@@ -5,7 +5,6 @@ public class Test
 	public static void main(String[] args)
 	{
 		System.out.println("hello,world");
-		System.out.println("hello,github");
-		System.out.println("test1");
+		//System.out.println("hello,github");
 	}
 }
